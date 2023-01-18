3b97c0dd0359bc4b0d69a5ebdde4f2dbf7462c82
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-01-18 12:07:15 -0500

    template routing

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  path: string;
+}
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
+
+  templateMenuItems: MenuItem[] = [
+    {
+      name: 'Basic',
+      path: 'template/basic',
+    },
+    {
+      name: 'Dynamic',
+      path: 'template/dymamic',
+    },
+    {
+      name: 'Switch',
+      path: 'template/switch',
+    }
+  ]
 
 }
