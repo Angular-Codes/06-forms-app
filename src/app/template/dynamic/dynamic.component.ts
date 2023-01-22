4a9ce38724fc92f49edc6e9e965a9043b013552e
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-01-21 22:48:31 -0500

    dynamic form ok

--- src/app/template/dynamic/dynamic.component.ts
+++ src/app/template/dynamic/dynamic.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  name      : string
+  favorites : Favorite[]
+}
+
+interface Favorite {
+  id  : number,
+  name: string
+}
+
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
@@ -7,8 +17,42 @@ import { Component } from '@angular/core';
 })
 export class DynamicComponent {
 
+  newGame:string = '';
+
+  person: Person = {
+    name: 'Kevin',
+    favorites: [
+      {
+        id: 1,
+        name: 'Metal Gear'
+      },
+      {
+        id: 2,
+        name: 'Mario Bros'
+      },
+      {
+        id: 3,
+        name: 'Fifa 2021'
+      }
+    ]
+  }
+
   save() {
     console.log('Form print');
+  }
+
+  addNewGame(){
+    const newGame: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newGame
+    }
+
+    this.person.favorites.push( {...newGame} );
+    this.newGame = '';
+  }
+
+  deleteFavorite( itemIndex: number ) {
+    this.person.favorites.splice(itemIndex, 1);
   }
 
 }
