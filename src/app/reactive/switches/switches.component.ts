83c34464fa9e346bc526775eea8cf42143dffeb7
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-01-28 13:14:52 -0500

    end finish reactive form

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface FormValue {
+  gender: string,
+  notifications: boolean,
+  conditions: boolean
+}
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+
+  myForm: FormGroup = this.fb.group({
+    'gender'        : [ '', Validators.required ],
+    'notifications' : [ false, Validators.required ],
+    'conditions'    : [ false, Validators.requiredTrue ],
+  })
+
+  person = {
+    gender: 'F',
+    notifications: true,
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.myForm.reset({...this.person})
+
+    // Suscribe when form change
+    this.myForm.valueChanges.subscribe( ( form: FormValue ) => {
+      const { conditions, ...rest } = { ...form } as FormValue
+      this.person = rest
+    })
+
+  }
+
+  savePerson() {
+    const { conditions, ...rest } = { ...this.myForm.value } as FormValue
+    this.person = rest
+  }
 
 }
