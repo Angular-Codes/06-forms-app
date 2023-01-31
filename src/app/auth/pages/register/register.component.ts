0f5f5ed3f9f77429fac87760950b1de9f3508d99
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-01-30 20:43:28 -0500

    register form

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  nameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)'
+
+  myForm: FormGroup = this.fb.group({
+    'name': ['', [ Validators.required, Validators.pattern( this.nameAndLastnamePattern ) ]]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  isNoValidField( field: string ){
+    return this.myForm.get(field)?.invalid && this.myForm.get(field)?.touched
+  }
 
 }
