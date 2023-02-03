22da5878895b8e7c8b4b005e0736ef6f965f1ec0
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-02-02 21:53:00 -0500

    finish project

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -11,6 +11,8 @@ import { EmailValidatorService } from '../../../shared/validations/email-validat
 })
 export class RegisterComponent {
 
+
+
   myForm: FormGroup = this.fb.group({
     name      : ['', [ Validators.required, Validators.pattern( this.formsValidatorService.nameAndLastnamePattern ) ]],
     email     : ['', [ Validators.required, Validators.pattern( this.formsValidatorService.emailPattern ) ], [ this.emailValidator ]],
@@ -21,6 +23,15 @@ export class RegisterComponent {
     validators: [ this.formsValidatorService.matchPassword('password', 'password2') ]
   })
 
+  get errorEmail(): string {
+    const errors = this.myForm.get('email')?.errors;
+    if      (errors?.['required'])    return 'Email is required'
+    else if (errors?.['pattern'])     return 'Email is not valid'
+    else if (errors?.['emailTaken'])  return 'Email is already taken'
+    return ''
+  }
+
+
   constructor(
     private fb: FormBuilder,
     private formsValidatorService: FormsValidatorService,
@@ -30,12 +41,16 @@ export class RegisterComponent {
       name      : 'Kevin cuadros',
       email     : 'test10@test.com',
       username  : 'kevin_cuadros_',
+      password  : '123456',
+      password2 : '123456',
     })
   }
 
   isNoValidField( field: string ){
     return this.myForm.get(field)?.invalid && this.myForm.get(field)?.touched
   }
+
+
 
   save(){
     if(this.myForm.invalid){
